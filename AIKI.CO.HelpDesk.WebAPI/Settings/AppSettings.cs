fc70cb8666445e17bcd8b7e9aededdfdf2b7782a
Mohammad Mehrnia <qermezkon@gmail.com>
2020-02-24 22:35:11 +0330

    Add JWT Authenticate

---
+++ AIKI.CO.HelpDesk.WebAPI/Settings/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIKI.CO.HelpDesk.WebAPI.Settings
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
