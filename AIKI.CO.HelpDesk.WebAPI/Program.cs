6414aae9e98655e5aba073b8d9ccfe81ce3c11e5
Mohammad Mehrnia <qermezkon@gmail.com>
2020-02-24 12:50:01 +0330

    Add project files.

---
+++ AIKI.CO.HelpDesk.WebAPI/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AIKI.CO.HelpDesk.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+           .ConfigureAppConfiguration((hostContext, configApp) =>
+           {
+               configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true);
+               configApp.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+               configApp.AddCommandLine(args);
+           })
+           .ConfigureWebHostDefaults(webBuilder =>
+           {
+               webBuilder.UseStartup<Startup>();
+           });
+    }
+}
