081bce4039d8ab9079638ec8a79379c955e78910
Mohammad Mehrnia <qermezkon@gmail.com>
2020-02-25 23:36:43 +0330

    -

---
+++ AIKI.CO.HelpDesk.WebAPI/Services/BaseService.cs
@@ -0,0 +1,22 @@
+﻿using AIKI.CO.HelpDesk.WebAPI.Settings;
+using Arch.EntityFrameworkCore.UnitOfWork;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIKI.CO.HelpDesk.WebAPI.Services
+{
+    public class BaseService
+    {
+        protected readonly AppSettings _appSettings;
+        protected readonly IUnitOfWork _unitofwork;
+
+        public BaseService(IUnitOfWork unitofwork, IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+            _unitofwork = unitofwork;
+        }
+    }
+}
