e90d97cc68c7b7e42fbdc1e0f87f7369fe22c172
Mohammad Mehrnia <qermezkon@gmail.com>
2020-02-25 23:28:10 +0330

    -

---
+++ AIKI.CO.HelpDesk.WebAPI/Models/EntitiesConfiguration/BaseViewConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AIKI.CO.HelpDesk.WebAPI.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIKI.CO.HelpDesk.WebAPI.Models.EntitiesConfiguration
+{
+    public class BaseViewConfiguration<T> : IEntityTypeConfiguration<T> where T : BaseObject
+    {
+        protected Guid _companyid { get; set; }
+        public BaseViewConfiguration(Guid companyid)
+        {
+            _companyid = companyid;
+        }
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<T> builder)
+        {
+            builder.HasNoKey();
+            builder.HasQueryFilter(p => p.companyid == _companyid);
+        }
+    }
+}
--- AIKI.CO.HelpDesk.WebAPI/Settings/AppSettings.cs
+++ AIKI.CO.HelpDesk.WebAPI/Settings/AppSettings.cs
@@ -8,5 +8,6 @@ namespace AIKI.CO.HelpDesk.WebAPI.Settings
     public class AppSettings
     {
         public string Secret { get; set; }
+        public Guid CompanyID { get; set; } = new Guid("997afb89-9abf-4889-8e43-cc301a311a9f");
     }
 }
