2dbce0f9866511faadacc99b476e119371b269c5
MohMehrnia <qermezkon@gmail.com>
2020-04-11 08:51:21 +0430

    Bug Fixed In AppConstantItemsController

--- AIKI.CO.HelpDesk.WebAPI/Controllers/AppConstantItemsController.cs
+++ AIKI.CO.HelpDesk.WebAPI/Controllers/AppConstantItemsController.cs
@@ -13,8 +13,6 @@ using Microsoft.Extensions.Options;
 
 namespace AIKI.CO.HelpDesk.WebAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
     public class AppConstantItemsController : BaseCRUDApiController<AppConstantItem, AppConstantItemResponse>
     {
         public AppConstantItemsController(
