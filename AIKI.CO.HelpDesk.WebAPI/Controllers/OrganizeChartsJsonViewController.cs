e957ef067d80c947203dd33ac6b4c1444d2e5e7c
MohMehrnia <qermezkon@gmail.com>
2020-04-14 10:32:46 +0430

    Add OrganizeCharts Json View Model & Controller

--- AIKI.CO.HelpDesk.WebAPI/Controllers/OrganizeChartsJsonViewController.cs
+++ AIKI.CO.HelpDesk.WebAPI/Controllers/OrganizeChartsJsonViewController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using AIKI.CO.HelpDesk.WebAPI.Models.Entities;
 using AIKI.CO.HelpDesk.WebAPI.Models.ReponseEntities;
 using AIKI.CO.HelpDesk.WebAPI.Services.Interface;
@@ -8,13 +10,22 @@ using Microsoft.Extensions.Options;
 
 namespace AIKI.CO.HelpDesk.WebAPI.Controllers
 {
-    public class OrganizeCharts_JsonViewController : BaseCRUDApiController<OrganizeCharts_JsonView, OrganizeCharts_JsonViewResponse>
+    public class OrganizeChartsJsonViewController : BaseCRUDApiController<OrganizeCharts_JsonView, OrganizeCharts_JsonViewResponse>
     {
-        public OrganizeCharts_JsonViewController(
+        public OrganizeChartsJsonViewController(
             IMapper map,
             IOptions<AppSettings> appSettings,
             IService<OrganizeCharts_JsonView, OrganizeCharts_JsonViewResponse> service) : base(map, appSettings, service,isReadOnly:true)
         {
+        }
+        
+        [HttpGet("GetByCompanyId/{id:guid}")]
+        public async Task<IActionResult> GetByCompanyId(Guid id)
+        {
+            var result = await _service.GetAll(predicate: q=>q.companyid == id);
+            if (result != null)
+                return Ok(result);
+            else return NotFound();
         }
     }
 }
