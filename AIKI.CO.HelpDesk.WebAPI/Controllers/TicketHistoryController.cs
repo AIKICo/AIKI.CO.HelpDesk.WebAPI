30d3ba4326e4d5a0deadabd0666caa7119752326
Mohammad Mehrnia <qermezkon@gmail.com>
2020-06-11 19:18:19 +0430

    Add TicketHistoryByTicketID

--- AIKI.CO.HelpDesk.WebAPI/Controllers/TicketHistoryController.cs
+++ AIKI.CO.HelpDesk.WebAPI/Controllers/TicketHistoryController.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Threading.Tasks;
 using AIKI.CO.HelpDesk.WebAPI.Models.Entities;
 using AIKI.CO.HelpDesk.WebAPI.Models.ReponseEntities;
 using AIKI.CO.HelpDesk.WebAPI.Services.Interface;
 using AIKI.CO.HelpDesk.WebAPI.Settings;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
 namespace AIKI.CO.HelpDesk.WebAPI.Controllers
@@ -14,6 +17,20 @@ namespace AIKI.CO.HelpDesk.WebAPI.Controllers
             IOptions<AppSettings> appSettings,
             IService<TicketHistory, TicketHistoryResponse> service) : base(map, appSettings, service)
         {
+        }
+
+        [HttpGet("TicketHistoryByTicketID")]
+        public async Task<IActionResult> TicketHistoryByTicketID(Guid id)
+        {
+            var response = await _service.GetAll(q => q.ticketid == id);
+            if (response != null)
+            {
+                return Ok(response);
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
     }
 }
