746e5fa0d01b3c99df2ab2187e0f6362e2e31755
Mohammad Mehrnia <qermezkon@gmail.com>
2020-06-30 12:51:07 +0430

    Bug Fixed in AppConstantItemConfiguration.cs

--- AIKI.CO.HelpDesk.WebAPI/Models/Entities/BaseObject.cs
+++ AIKI.CO.HelpDesk.WebAPI/Models/Entities/BaseObject.cs
@@ -6,6 +6,6 @@ namespace AIKI.CO.HelpDesk.WebAPI.Models.Entities
     public class BaseObject
     {
         public Guid id { get; set; }
-        public Guid? companyid { get; set; }
+        public Guid companyid { get; set; }
     }
 }
--- AIKI.CO.HelpDesk.WebAPI/Models/ReponseEntities/BaseResponse.cs
+++ AIKI.CO.HelpDesk.WebAPI/Models/ReponseEntities/BaseResponse.cs
@@ -5,6 +5,6 @@ namespace AIKI.CO.HelpDesk.WebAPI.Models.ReponseEntities
     public class BaseResponse
     {
         public Guid id { get; set; }
-        public Guid? companyid { get; set; }
+        public Guid companyid { get; set; }
     }
 }
