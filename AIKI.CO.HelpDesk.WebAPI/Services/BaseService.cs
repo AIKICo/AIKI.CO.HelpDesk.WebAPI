75f4231b1fc2e50e78526de223102a8c5648ca51
Mohammad Mehrnia <qermezkon@gmail.com>
2020-07-03 02:20:59 +0430

    -

--- AIKI.CO.HelpDesk.WebAPI/Services/BaseService.cs
+++ AIKI.CO.HelpDesk.WebAPI/Services/BaseService.cs
@@ -47,7 +47,7 @@ namespace AIKI.CO.HelpDesk.WebAPI.Services
             _protector = provider.CreateProtector("MemberService.CompanyId");
             if (_context.HttpContext.Request.Headers["CompanyID"].Any())
             {
-                _companyId = new Guid(_protector.Unprotect(_context.HttpContext.Request.Headers["CompanyID"].ToString()));
+                _companyId = Guid.Parse(_protector.Unprotect(_context.HttpContext.Request.Headers["CompanyID"].ToString()));
             }
         }
 
