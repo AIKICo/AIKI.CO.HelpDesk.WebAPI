5fbd752b8909d376b67cf80a939849d3ae783448
Mohammad Mehrnia <qermezkon@gmail.com>
2020-07-05 08:42:37 +0430

    -

--- AIKI.CO.HelpDesk.WebAPI/Services/CompanyService.cs
+++ AIKI.CO.HelpDesk.WebAPI/Services/CompanyService.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using AIKI.CO.HelpDesk.WebAPI.Models.Entities;
 using AIKI.CO.HelpDesk.WebAPI.Models.ReponseEntities;
 using AIKI.CO.HelpDesk.WebAPI.Services.Interface;
 using Arch.EntityFrameworkCore.UnitOfWork;
 using AutoMapper;
+using AutoMapper.Internal;
 using Microsoft.AspNetCore.Authorization;
 
 namespace AIKI.CO.HelpDesk.WebAPI.Services
@@ -24,6 +26,26 @@ namespace AIKI.CO.HelpDesk.WebAPI.Services
             request.id = Guid.NewGuid();
             await _unitofwork.GetRepository<Company>().InsertAsync(_map.Map<Company>(request));
             await _unitofwork.SaveChangesAsync();
+            
+            // Add Constant
+            var AppConstantRepo = _unitofwork.GetRepository<AppConstant>();
+            var AppContants = await AppConstantRepo.GetAllAsync(q => q.companyid == Guid.Parse("997afb89-9abf-4889-8e43-cc301a311a9f"), ignoreQueryFilters:true);
+            AppContants.ForAll(async (record) =>
+            {
+                record.companyid = request.id;
+                await AppConstantRepo.InsertAsync(record);
+            });
+            await _unitofwork.SaveChangesAsync();
+
+            var AppConstantItemRepo = _unitofwork.GetRepository<AppConstantItem>();
+            var AppContantItems = await AppConstantItemRepo.GetAllAsync(q => q.companyid == Guid.Parse("997afb89-9abf-4889-8e43-cc301a311a9f"), ignoreQueryFilters:true);
+            AppContantItems.ForAll(async (record) =>
+            {
+                record.companyid = request.id;
+                await AppConstantItemRepo.InsertAsync(record);
+            });
+            await _unitofwork.SaveChangesAsync();
+            
             return request;
         }
     }
