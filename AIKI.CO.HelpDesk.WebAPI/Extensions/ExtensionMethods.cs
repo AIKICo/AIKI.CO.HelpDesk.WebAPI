2366cb95451002700d6a6c0979268b89ce340009
Mohammad Mehrnia <qermezkon@gmail.com>
2020-07-13 08:50:38 +0430

    Security Bug Fixed In Return Members Info

--- AIKI.CO.HelpDesk.WebAPI/Extensions/ExtensionMethods.cs
+++ AIKI.CO.HelpDesk.WebAPI/Extensions/ExtensionMethods.cs
@@ -12,9 +12,19 @@ namespace AIKI.CO.HelpDesk.WebAPI.Extensions
             return members.Select(x => x.WithoutPassword());
         }
 
+        public static IEnumerable<MemberResponse> WithoutCompanyIds(this IEnumerable<MemberResponse> members)
+        {
+            return members.Select(x => x.WithoutCompanyId());
+        }
         public static MemberResponse WithoutPassword(this MemberResponse user)
         {
             user.password = string.Empty;
+            return user;
+        }
+        
+        public static MemberResponse WithoutCompanyId(this MemberResponse user)
+        {
+            user.companyid = null;
             return user;
         }
     }
