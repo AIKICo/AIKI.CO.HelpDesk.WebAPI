61b77b607827ce3232292f24a3d7ef91c7538b53
Mohammad Mehrnia <qermezkon@gmail.com>
2020-07-15 08:58:50 +0430

    -

--- AIKI.CO.HelpDesk.WebAPI/Models/Entities/TargetsPriority.cs
+++ AIKI.CO.HelpDesk.WebAPI/Models/Entities/TargetsPriority.cs
@@ -9,6 +9,8 @@ namespace AIKI.CO.HelpDesk.WebAPI.Models.Entities
     {
         public string Title { get; set; }
         public string ResponseTime { get; set; }
+        public string ResponseTimeUnit { get; set; }
         public string ResolveTime { get; set; }
+        public string ResolveTimeUnit { get; set; }
     }
 }
