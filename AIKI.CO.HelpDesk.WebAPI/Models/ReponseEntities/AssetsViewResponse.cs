78ea8182ab7a929d383cff10f318d6518cd5a3f5
Mohammad Mehrnia <qermezkon@gmail.com>
2020-08-11 08:01:03 +0430

    -

--- AIKI.CO.HelpDesk.WebAPI/Models/ReponseEntities/AssetsViewResponse.cs
+++ AIKI.CO.HelpDesk.WebAPI/Models/ReponseEntities/AssetsViewResponse.cs
@@ -8,6 +8,7 @@ namespace AIKI.CO.HelpDesk.WebAPI.Models.ReponseEntities
         public string title { get; set; }
         public string assetlocationid { get; set; }
         public string assettypeid { get; set; }
+        public DateTime? deliverydate { get; set; }
         public Guid? customerid { get; set; }
     }
 }
