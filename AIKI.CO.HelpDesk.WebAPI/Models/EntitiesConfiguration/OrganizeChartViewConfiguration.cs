32af87f30533cea1ba1a1219756a522caeca2bec
Mohammad Mehrnia <qermezkon@gmail.com>
2020-09-13 23:07:12 +0430

    add children field column type in OrganizeChartViewConfiguration.cs

--- AIKI.CO.HelpDesk.WebAPI/Models/EntitiesConfiguration/OrganizeChartViewConfiguration.cs
+++ AIKI.CO.HelpDesk.WebAPI/Models/EntitiesConfiguration/OrganizeChartViewConfiguration.cs
@@ -1,5 +1,7 @@
 using System;
 using AIKI.CO.HelpDesk.WebAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AIKI.CO.HelpDesk.WebAPI.Models.EntitiesConfiguration
 {
@@ -7,6 +9,13 @@ namespace AIKI.CO.HelpDesk.WebAPI.Models.EntitiesConfiguration
     {
         public OrganizeChartViewConfiguration(Guid companyid) : base(companyid)
         {
+        }
+
+        public override void Configure(EntityTypeBuilder<OrganizeChartView> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(c => c.children).HasColumnType("json");
         }
     }
 }
