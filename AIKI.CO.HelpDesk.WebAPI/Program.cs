e4f5e652c5659105f21831fdd271bc3184040c3e
Mohammad Mehrnia <qermezkon@gmail.com>
2020-10-10 12:01:38 +0330

    -

--- AIKI.CO.HelpDesk.WebAPI/Program.cs
+++ AIKI.CO.HelpDesk.WebAPI/Program.cs
@@ -9,7 +9,9 @@ namespace AIKI.CO.HelpDesk.WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            //MigrateDatabase(host);
+            host.Run();
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args)
@@ -28,5 +30,43 @@ namespace AIKI.CO.HelpDesk.WebAPI
                     webBuilder.UseSentry();
                 });
         }
+
+/*
+        private static void MigrateDatabase(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<Models.dbContext>();
+            var databaseIsUp = false;
+            while (!databaseIsUp)
+            {
+                try
+                {
+                    var isFirstRun = dbContext.Database.CanConnect() == false;
+                    databaseIsUp = true;
+
+                    // migrate database
+                    dbContext.Database.Migrate();
+                    Console.WriteLine("[DATABASE] Migration(s) were applied.");
+
+                    if (isFirstRun)
+                    {
+                        // seed
+                    }
+                }
+                catch (Npgsql.NpgsqlException npgex)
+                {
+                    if (npgex.InnerException is SocketException ||
+                        (!string.IsNullOrWhiteSpace(npgex.Message) && npgex.Message.StartsWith("57P03")))
+                    {
+                        // wait a moment for next try
+                        Console.WriteLine("[DATABASE] Database service is not running yet. Trying to connect again ...");
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                    else throw;
+                }
+            }
+        }
+*/
+
     }
 }
